474d9ce1c4f85f06b9d3f1388c6102d0dc28168c
red-green-blume <mystandartmail@gmai.com>
2025-11-30 20:26:48 +0300

    init

---
+++ src/main/java/com/slumpy/team/BdHw2Application.java
@@ -0,0 +1,13 @@
+package com.slumpy.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdHw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdHw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/slumpy/team/BdHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slumpy.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdHw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
